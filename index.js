2fddd17891fb7183433ff063ddb225491105ae0c
Adam Wilder <wilderav86@gmail.com>
2022-11-17 11:08:22 -0800

    styling changes

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const getData = async () => {
-  let response = await fetch("/data.json");
-  let data = await response.json();
-  console.log(data);
+const url = "/data.json";
+
+// Elements
+const graph = document.querySelector(".graph");
+
+// Data
+const getData = async (url) => {
+  response = await fetch(url);
+  data = await response.json();
+  return data;
 };
 
-getData();
+//Functions
+const renderGraph = (data) => {
+  data.forEach((day) => {
+    const barContainer = `<div class="bar-container">
+        <div class="total">$${day.amount}</div>
+        <div class="bar" style="height:${day.amount * 2.7}px"></div>
+        <div class="day">${day.day}</div>
+      </div>`;
+
+    graph.innerHTML += barContainer;
+  });
+  console.log(graph, data);
+};
+
+getData(url).then(renderGraph);
